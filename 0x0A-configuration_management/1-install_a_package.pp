d71f1411f72de5afad29f39ef33afb0e68f3b82c
daadesina <daadesina1@gmail.com>
2024-04-24 18:02:37 +0100

    adesina

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Install a package
+# Install a package from pip3 - flask version 2.1.0
 package {'flask':
-ensure   => '2.1.0',
-provider => 'pip3'
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
